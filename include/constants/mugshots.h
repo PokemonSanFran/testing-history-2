64ce42b8be566f6bd43d11cb45463203bca14823
PSF <pkmnsnfrn@gmail.com>
2021-09-11 15:24:19 -0700

    setting up mugshot constants from pokecommunity

---
+++ include/constants/mugshots.h
@@ -0,0 +1,8 @@
+#ifndef GUARD_CONSTANTS_MUGSHOTS_H
+#define GUARD_CONSTANTS_MUGSHOTS_H
+
+//Add your mugshot constants here
+#define MUGSHOT_
+
+//https://www.pokecommunity.com/showpost.php?p=10345947&postcount=252
+#endif
