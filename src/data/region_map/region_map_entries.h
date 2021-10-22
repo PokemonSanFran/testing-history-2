853caf5d2229fd06adcff0ddbd0d4568148952cf
PSF <pkmnsnfrn@gmail.com>
2021-10-21 19:39:27 -0700

    finished .3, saving function works correctly

--- src/data/region_map/region_map_entries.h
+++ src/data/region_map/region_map_entries.h
@@ -198,7 +198,7 @@ static const u8 sMapName_MarineCave[] = _("Marine Cave");
 static const u8 sMapName_TerraCave[] = _("Terra Cave");
 static const u8 sMapName_DesertUnderpass[] = _("Desert Underpass");
 static const u8 sMapName_TrainerHill[] = _("Trainer Hill");
-static const u8 sMapName_Orre[] = _("Orre");
+static const u8 sMapName_Orre[] = _("Agate Village");
 
 const struct RegionMapLocation gRegionMapEntries[] = {
     [MAPSEC_LITTLEROOT_TOWN]            = { 4, 11, 1, 1, sMapName_LittlerootTown},
@@ -414,7 +414,7 @@ const struct RegionMapLocation gRegionMapEntries[] = {
     [MAPSEC_ALTERING_CAVE]              = { 6,  8, 1, 1, sMapName_AlteringCave},
     [MAPSEC_NAVEL_ROCK]                 = { 0,  0, 1, 1, sMapName_NavelRock},
     [MAPSEC_TRAINER_HILL]               = { 8,  4, 1, 1, sMapName_TrainerHill},
-    [MAPSEC_ORRE]                        = { 0,  0, 1, 1, sMapName_Orre}
+    [MAPSEC_ORRE]                       = { 0,  0, 1, 1, sMapName_Orre}
 };
 
 #endif //GUARD_DATA_REGION_MAP_REGION_MAP_ENTRIES_H
