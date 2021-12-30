f3f450076b42a32008b4ab0fe4f0312c1165282a
PSF <pkmnsnfrn@gmail.com>
2021-12-30 14:38:14 -0800

    You Look Tired 100%

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -57,7 +57,7 @@
 #define FLAG_HIDE_SORRYFRIENDS_PSFGOODADMIN 0x2D // Previously Unused Flag
 #define FLAG_HIDE_SWAGBAG2_PSFBADADMIN1 0x2E// Previously Unused Flag
 #define FLAG_HIDE_STORYSOFAR_PSFELITE1    0x2F // Previously Unused Flag
-#define FLAG_UNUSED_0x030    0x30 // Unused Flag
+#define FLAG_HIDE_YOULOOKTIRED_PSFBADADMIN2 0x30 // Previously Unused Flag
 #define FLAG_UNUSED_0x031    0x31 // Unused Flag
 #define FLAG_UNUSED_0x032    0x32 // Unused Flag
 #define FLAG_UNUSED_0x033    0x33 // Unused Flag
--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -654,4 +654,6 @@
 
 #define MAP_GROUPS_COUNT 40
 
+// static const u8 MAP_GROUP_COUNT[] = {89, 3, 1, 1, 1, 2, 1, 5, 5, 6, 7, 8, 9, 7, 7, 14, 8, 17, 10, 23, 13, 15, 15, 2, 2, 2, 3, 1, 1, 1, 115, 61, 90, 2, 1, 13, 1, 1, 3, 1, 0};
+
 #endif // GUARD_CONSTANTS_MAP_GROUPS_H
