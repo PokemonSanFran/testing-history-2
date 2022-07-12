dbf49c6a6f26a8820823c62642c06e76c901856f
PSF <pkmnsnfrn@gmail.com>
2022-07-12 08:40:56 -0700

    Alpha region map image added, no location labels added

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -357,7 +357,7 @@ graphics/pokemon_jump/bg.4bpp: %.4bpp: %.png
 
 graphics/pokenav/region_map.8bpp: %.8bpp: %.png
 	#$(GFX) $< $@ -num_tiles 233
-	$(GFX) $< $@ -num_tiles 233
+	$(GFX) $< $@ -num_tiles 128
 
 $(MISCGFXDIR)/japanese_hof.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -num_tiles 29
