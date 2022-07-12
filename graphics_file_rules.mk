f62f37dea422104e8cb26b519bfcdcf766e41e2c
PSF <pkmnsnfrn@gmail.com>
2022-07-12 08:13:00 -0700

    Move Pokenav flag to 1st debug script and preapre graphic_file_rules for new variable

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -356,6 +356,7 @@ graphics/pokemon_jump/bg.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -num_tiles 63
 
 graphics/pokenav/region_map.8bpp: %.8bpp: %.png
+	#$(GFX) $< $@ -num_tiles 233
 	$(GFX) $< $@ -num_tiles 233
 
 $(MISCGFXDIR)/japanese_hof.4bpp: %.4bpp: %.png
