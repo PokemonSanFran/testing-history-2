21db87167e4809eaafb415c1b57aa996ead57c15
pkmnsnfrn <pkmnsnfrn@gmail.com>
2022-08-27 21:07:47 -0700

    finished angel delivery

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -93,6 +93,7 @@
 #define MAP_PACIFICA_GEMHOUSE                 (84 | (0 << 8))
 #define MAP_TENDERLOIN_TROLLEY_HQ             (85 | (0 << 8))
 #define MAP_MARIN_SOLAR                       (86 | (0 << 8))
+#define MAP_DOGPATCH_BIKERHQ                  (87 | (0 << 8))
 
 // gMapGroup_TownsAndRoutes
 #define MAP_PETALBURG_CITY      (0 | (1 << 8))
