5973feebd910e6ef0b2989cdfa00ad59caf99577
pkmnsnfrn <pkmnsnfrn@gmail.com>
2023-06-12 11:53:46 -0700

    Added cabbies battles

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -907,6 +907,7 @@
 #define TRAINER_SIEBOLD_ROBOT_3             158
 #define TRAINER_SIEBOLD_ROBOT_4             159
 #define TRAINER_GERTRUDE                    160
+#define TRAINER_CABBIE_HALE                 161
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
