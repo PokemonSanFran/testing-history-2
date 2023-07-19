360ac188ca9e71ab936f36cd7f2fbbf93ba750c4
PSF <pkmnsnfrn@gmail.com>
2023-07-18 19:35:01 -0700

    Added Quest_Brucelee_UpdateType to header of questlogic for the clock

--- include/quest_logic.h
+++ include/quest_logic.h
@@ -5,5 +5,6 @@ void Quest_Kitchenvolunteering_CreatePantryMaze(void);
 u8 Quest_Kitchenvolunteering_CountRemainingItems(void);
 void Quest_Rockcollector_RespawnStones(void);
 u32 GetCurrentMap(void);
+bool8 Quest_Brucelee_UpdateType(u16 days);
 
 #endif //GUARD_QUESTLOGIC_H
